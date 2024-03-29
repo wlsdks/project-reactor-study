9ed966abaaa5fbd6283c1f2a74fcfffe06a9bfb7
wlsdks <wlsdks12@naver.com>
2024-03-29 16:25:14 +0900

    feat: Flux, Mono 간단히 살펴보기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var publisher = new Publisher();
+
+//        publisher.startFlux()
+//                .subscribe(System.out::println);
+
+//        publisher.startMono()
+//                .subscribe();
+
+        publisher.startMono2().subscribe();
     }
 }
---
+++ src/main/java/org/example/Publisher.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class Publisher {
+
+    public Flux<Integer> startFlux() {
+//        return Flux.range(1,10);
+        return Flux.range(1,10).log();
+    }
+
+    public Mono<Integer> startMono() {
+//        return Mono.just(1);
+        return Mono.just(1).log();
+    }
+
+    public Mono<?> startMono2() {
+        return Mono.empty().log();
+    }
+
+}
