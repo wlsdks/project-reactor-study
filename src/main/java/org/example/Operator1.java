af04211d5693f5abc8105dcd94a6d9c11d484739
wlsdks <wlsdks12@naver.com>
2024-03-29 16:53:48 +0900

    feat: map, filter, take, flatMap 연습

---
+++ src/main/java/org/example/Operator1.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import reactor.core.publisher.Flux;
+
+public class Operator1 {
+
+    // map
+    public Flux<Integer> fluxMap() {
+        return Flux.range(1, 5)
+                .map(i -> i * 2)
+                .log();
+    }
+
+    // filter
+    public Flux<Integer> fluxFilter() {
+        return Flux.range(1, 10)
+                .filter(i -> i > 5)
+                .log();
+    }
+
+    // take
+    public Flux<Integer> fluxFilterTake() {
+        return Flux.range(1, 10)
+                .filter(i -> i > 5)
+                .take(3) // 3개만 가져와라
+                .log();
+    }
+
+    // flatMap
+    public Flux<Integer> fluxFlatMap() {
+        return Flux.range(1, 10)
+                .flatMap(i -> Flux.range(i * 10, 10))
+                .log();
+    }
+
+    // flatMap2
+    public Flux<Integer> fluxFlatMap2() {
+        //for (int i=0; ... )
+        //   for (int j=0; ... )
+        return Flux.range(1, 9)
+                .flatMap(i -> Flux.range(1, 9)
+                        .map(j -> {
+                            System.out.printf("%d * %d = %d\n", i, j, i * j);
+                            return i * j;
+                        })
+                )
+                .log();
+    }
+
+}
